21a09440f923ca26a3653ca190465c257d92e659
Jonathan Manahan (RIT Student) <jam9858@ad.rit.edu>
2018-11-08 13:55:38 -0500

    Updating files to initial starting place

--- pwm.c
+++ pwm.c
@@ -8,13 +8,15 @@
  * Modified: 3/07/2015
  */
 #include "MK64F12.h"
-#include "Lab_6_pwm.h"
+#include "pwm.h"
 
 /*From clock setup 0 in system_MK64f12.c*/
 #define DEFAULT_SYSTEM_CLOCK 20485760u /* Default System clock value */
 #define CLOCK					20485760u
 #define PWM_FREQUENCY			10000
+#define SERVO_FREQ        50
 #define FTM0_MOD_VALUE			(CLOCK/PWM_FREQUENCY)
+#define FTM2_MOD_VALUE      (CLOCK/SERVO_FREQ)/128
 
 static volatile unsigned int PWMTick = 0;
 
@@ -39,6 +41,15 @@ void SetDutyCycle(unsigned int DutyCycle, unsigned int Frequency, int dir)
 	FTM0_MOD = (CLOCK/Frequency);
 }
 
+void SetDutyCycleServo(unsigned int DutyCycle)
+{
+	// Calculate the new cutoff value
+	uint16_t mod = (uint16_t) ((FTM2_MOD_VALUE * DutyCycle) / 100);
+  
+	// Update the clock to the new frequency
+	FTM2_C0V = (mod);
+}
+
 /*
  * Initialize the FlexTimer for PWM
  */
@@ -46,6 +57,7 @@ void InitPWM()
 {
 	// 12.2.13 Enable the Clock to the FTM0 Module
 	SIM_SCGC6 |= SIM_SCGC6_FTM0_MASK;
+	SIM_SCGC6 |= SIM_SCGC6_FTM2_MASK;
 	
 	// Enable clock on PORT A so it can output
 	SIM_SCGC5 |= SIM_SCGC5_PORTA_MASK | SIM_SCGC5_PORTB_MASK | SIM_SCGC5_PORTC_MASK;
@@ -54,21 +66,27 @@ void InitPWM()
 	// Use drive strength enable flag to high drive strength
 	//These port/pins may need to be updated for the K64 <Yes, they do. Here are two that work.>
 	
-    PORTC_PCR3  = PORT_PCR_MUX(4)  | PORT_PCR_DSE_MASK; //Ch2
-    PORTC_PCR4  = PORT_PCR_MUX(4)  | PORT_PCR_DSE_MASK;//Ch3
+  PORTC_PCR3  = PORT_PCR_MUX(4)  | PORT_PCR_DSE_MASK; //Ch2
+  PORTC_PCR4  = PORT_PCR_MUX(4)  | PORT_PCR_DSE_MASK;//Ch3
+	
+	PORTB_PCR18 = PORT_PCR_MUX(3)  | PORT_PCR_DSE_MASK; //FTM2 Ch0
 	
 	// 39.3.10 Disable Write Protection
 	FTM0_MODE |= FTM_MODE_WPDIS_MASK;
+	FTM2_MODE |= FTM_MODE_WPDIS_MASK;
 	
 	// 39.3.4 FTM Counter Value
 	// Initialize the CNT to 0 before writing to MOD
 	FTM0_CNT = 0;
+	FTM2_CNT = 0;
 	
 	// 39.3.8 Set the Counter Initial Value to 0
 	FTM0_CNTIN = 0;
+	FTM2_CNTIN = 0;
 	
 	// 39.3.5 Set the Modulo resister
 	FTM0_MOD = FTM0_MOD_VALUE;
+	FTM2_MOD = FTM2_MOD_VALUE;
 	//FTM0->MOD = (DEFAULT_SYSTEM_CLOCK/(1<<7))/1000;
 
 	// 39.3.6 Set the Status and Control of both channels
@@ -81,19 +99,27 @@ void InitPWM()
 	FTM0_C2SC |= FTM_CnSC_MSB_MASK | FTM_CnSC_ELSB_MASK;
 	FTM0_C2SC &= ~FTM_CnSC_ELSA_MASK;
 	
+	// Same for Channel 0 FTM2
+	FTM2_C0SC |= FTM_CnSC_MSB_MASK | FTM_CnSC_ELSB_MASK;
+	FTM2_C0SC &= ~FTM_CnSC_ELSA_MASK;
 	// 39.3.3 FTM Setup
 	// Set prescale value to 1 
 	// Chose system clock source
 	// Timer Overflow Interrupt Enable
 	FTM0_SC = FTM_SC_PS(0) | FTM_SC_CLKS(1); 
 	//| FTM_SC_TOIE_MASK;
+	
+	// Set prescale value to 128 
+	// Chose system clock source
+	// Timer Overflow Interrupt Enable
+	FTM2_SC = FTM_SC_PS(7) | FTM_SC_CLKS(1); 
 
 	// Enable Interrupt Vector for FTM
-    //NVIC_EnableIRQ(FTM0_IRQn);
+  //NVIC_EnableIRQ(FTM0_IRQn);
 
 }
 
-/*OK to remove this ISR?*/
+/*OK to remove this ISR*/
 void FTM0_IRQHandler(void){ //For FTM timer
 
   FTM0_SC &= ~FTM_SC_TOF_MASK;
