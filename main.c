2f5c19a4b26d36050097b80454c545290fee50ee
Jamie <jrk1443@rit.edu>
2018-11-16 01:36:46 -0500

    variable speed implemented

--- main.c
+++ main.c
@@ -13,6 +13,7 @@
 #include "camera_FTM.h"
 #include "isr.h"
 #include <string.h>
+#include <math.h>
 
 // Default System clock value
 // period = 1/20485760  = 4.8814395e-8
@@ -30,7 +31,9 @@ int main(void)
 { 
   uint16_t freq = 10000; /* Frequency = 10 kHz */
   uint16_t cam_data[128];
+  double speed = 40;
   double duty = 0;
+  double old_duty = 0;
   char string[100];
   
 	// Initialize UART and PWM
@@ -48,20 +51,25 @@ int main(void)
     memcpy(cam_data, (void*)getLine(), sizeof(cam_data));
     filter(cam_data);
 		
-	  SetDutyCycle(30, freq, 1, 2);
+    SetDutyCycle(speed, freq, 1, 2);
 		
 		for (int i = 0; i<123; i++)
 		{
 			if(cam_data[i])
       {
-        duty += -9.0*i + 549.0;
+        duty += -12.2*i + 744.2;
       }
 		}
     
-    sprintf(string, "\n\rduty %f\n\r", duty/7564.0+7.75);
-    put(string);
-    SetDutyCycleServo(duty/7564.0+7.75);
+    speed = 70 - fabs(duty)*35.0/15274.3;
     
+    //sprintf(string, "\n\rduty %f oldduty %f newduty %f\n\r", duty, old_duty, (duty+0.45*old_duty)/15274.3+7.75);
+    //put(string);
+
+    duty = duty + (duty - old_duty)*0.7;
+    SetDutyCycleServo((duty)/15270+7.75);
+    
+    old_duty = duty;
     duty = 0;
 	}
   return 0;
